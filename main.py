a762ae50c402cbaa7856fce8f240777295277915
felix <bhunao@gmail.com>
2023-06-07 16:59:46 -0300

    enemies

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import pyxel
 
+from dataclasses import dataclass
+
 SPRITEDOWN = 0, 0, 0, 8, 8, 0
 SPRITEUP = 0, 8, 0, 8, 8, 0
 SPRITELEFT = 0, 0, 8, 8, 8, 0
 SPRITERIGHT = 0, 8, 8, 8, 8, 0
+
+
+def random_walk(character):
+    character.x = (character.x - pyxel.rndi(-1, 1)) % pyxel.width
+    character.y = (character.y - pyxel.rndi(-1, 1)) % pyxel.width
+
+
+@dataclass
+class Enemie:
+    name: str
+    x: int
+    y: int
+    sprite: int
 
 class Player:
     def __init__(self):
@@ -14,6 +29,10 @@ class Player:
 class App:
     def __init__(self):
         self.player = Player()
+        self.enemies = []
+
+        self.enemies.append(Enemie("inimigo", 10, 10, SPRITELEFT))
+
         pyxel.init(160, 120)
         pyxel.load("assets/pyxel.pyxres")
         pyxel.run(self.update, self.draw)
@@ -32,9 +51,15 @@ class App:
             self.player.y = (self.player.y - 1) % pyxel.height
             self.player.sprite = SPRITEUP
 
+        for enemie in self.enemies:
+            random_walk(enemie)
+
     def draw(self):
         pyxel.cls(0)
         pyxel.blt(self.player.x, self.player.y, *self.player.sprite)
+        
+        for enemie in self.enemies:
+            pyxel.blt(enemie.x, enemie.y, *enemie.sprite)
         
 
 App()
