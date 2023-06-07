f5cbe45676fb1167fda3bd518eb975672ec67a5a
Bruno <71756353+Brun1o@users.noreply.github.com>
2023-06-07 15:51:24 -0300

    Sprites

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pyxel
 class App:
     def __init__(self):
         pyxel.init(160, 120)
+        pyxel.load("assets/pyxel.pyxres")
         self.x = 0
         pyxel.run(self.update, self.draw)
 
@@ -11,6 +12,5 @@ class App:
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.rect(self.x, 0, 8, 8, 9)
-
+        pyxel.bltm(self.x, 0, 0, 0, 0, 8, 8, 0)
 App()
