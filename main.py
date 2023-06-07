b22c3978d34ca69f8a6c01c49459b5503bfd9dae
Bruno <71756353+Brun1o@users.noreply.github.com>
2023-06-07 16:29:49 -0300

    Movment

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 import pyxel
 
+SPRITEDOWN = 0, 0, 0, 8, 8, 0
+SPRITEUP = 0, 8, 0, 8, 8, 0
+SPRITELEFT = 0, 0, 8, 8, 8, 0
+SPRITERIGHT = 0, 8, 8, 8, 8, 0
+
+class Player:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.sprite = SPRITEDOWN
+
 class App:
     def __init__(self):
+        self.player = Player()
         pyxel.init(160, 120)
         pyxel.load("assets/pyxel.pyxres")
-        self.x = 0
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        self.x = (self.x + 1) % pyxel.width
+        if pyxel.btn(pyxel.KEY_LEFT):
+            self.player.x = (self.player.x - 1) % pyxel.width
+            self.player.sprite = SPRITELEFT
+        if pyxel.btn(pyxel.KEY_RIGHT):
+            self.player.x = (self.player.x + 1) % pyxel.width
+            self.player.sprite = SPRITERIGHT
+        if pyxel.btn(pyxel.KEY_DOWN):
+            self.player.y = (self.player.y + 1) % pyxel.height
+            self.player.sprite = SPRITEDOWN
+        if pyxel.btn(pyxel.KEY_UP):
+            self.player.y = (self.player.y - 1) % pyxel.height
+            self.player.sprite = SPRITEUP
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.bltm(self.x, 0, 0, 0, 0, 8, 8, 0)
+        pyxel.blt(self.player.x, self.player.y, *self.player.sprite)
+        
+
 App()
