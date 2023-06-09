229999c3407b66f41c18f618148938a1e3c134b9
Bruno <71756353+Brun1o@users.noreply.github.com>
2023-06-09 13:33:12 -0300

    things

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyxel
+import math
 
 from map_gen import map_seed
 from entity import Entity
@@ -28,15 +29,24 @@ def random_walk(character):
     character.x = (character.x - pyxel.rndi(-1, 1)) % pyxel.width
     character.y = (character.y - pyxel.rndi(-1, 1)) % pyxel.width
 
+def verifyCollision(objeto1, objeto2):
+        if (objeto1.x < objeto2.x + objeto2.sprite[3] and
+                objeto1.x + objeto1.sprite[3] > objeto2.x and
+                objeto1.y < objeto2.y + objeto2.sprite[4] and
+                objeto1.y + objeto1.sprite[4] > objeto2.y):
+            return True
+        else:
+            return False
 
 
 
 class App:
     def __init__(self):
-        self.player = Entity("Player", 0, 0, SPRITEDOWN)
+        self.player = Entity("Player", 80, 60, SPRITEDOWN)
         self.entities = []
-
+        self.vida = 10
         self.entities.append(Entity("inimigo", 10, 10, ENEMIE_1_DOWN))
+        self.last_key_pressed = None
 
         pyxel.init(160, 120)
         pyxel.load("assets/pyxel.pyxres")
@@ -48,19 +58,36 @@ class App:
         if pyxel.btn(pyxel.KEY_LEFT):
             self.player.x = (self.player.x - 1) % pyxel.width
             self.player.sprite = SPRITELEFT
+            self.last_key_pressed = pyxel.KEY_LEFT
         if pyxel.btn(pyxel.KEY_RIGHT):
             self.player.x = (self.player.x + 1) % pyxel.width
             self.player.sprite = SPRITERIGHT
+            self.last_key_pressed = pyxel.KEY_RIGHT
         if pyxel.btn(pyxel.KEY_DOWN):
             self.player.y = (self.player.y + 1) % pyxel.height
             self.player.sprite = SPRITEDOWN
+            self.last_key_pressed = pyxel.KEY_DOWN
         if pyxel.btn(pyxel.KEY_UP):
             self.player.y = (self.player.y - 1) % pyxel.height
             self.player.sprite = SPRITEUP
+            self.last_key_pressed = pyxel.KEY_UP
+        if pyxel.btn(pyxel.KEY_A):
+            self.draw_player_attack()
 
         for entity in self.entities:
             if entity.name == "inimigo":
                 random_walk(entity)
+        for entity in self.entities:
+            if entity.name == "inimigo" and verifyCollision(self.player, entity):
+                if self.player.x < entity.x:
+                    self.vida -= 1
+                    self.player.x -= 5
+                    entity.x += 5
+                else:
+                    self.vida -= 1
+                    self.player.x += 5
+                    entity.x -= 5
+                    
 
     def draw(self):
         pyxel.cls(1)
@@ -69,7 +96,50 @@ class App:
         for entity in self.entities:
             pyxel.blt(entity.x, entity.y, *entity.sprite)
 
-        pyxel.text(10, 10, "map_gen", 3)
+        vida_texto = "Vida: {}".format(self.vida)
+        pyxel.text(10, 10, vida_texto, 7)
+
+    def draw_player_attack(self):
+        pyxel.cls(2)
+        angle = 0
+
+        if self.last_key_pressed == pyxel.KEY_UP:
+            angle = 270
+        elif self.last_key_pressed == pyxel.KEY_DOWN:
+            angle = 90
+        elif self.last_key_pressed == pyxel.KEY_LEFT:
+            angle = 180
+        elif self.last_key_pressed == pyxel.KEY_RIGHT:
+            angle = 0
+
+        # Converter o ângulo para radianos
+        angle_rad = math.radians(angle)
+
+        # Calcular as coordenadas finais do raio de ataque
+        attack_length = 40
+        attack_end_x = self.player.x + attack_length * math.cos(angle_rad)
+        attack_end_y = self.player.y + attack_length * math.sin(angle_rad)
+
+        rect_width = abs(attack_end_x - self.player.x)
+        rect_height = abs(attack_end_y - self.player.y)
+        rect_x = min(attack_end_x, self.player.x)
+        rect_y = min(attack_end_y, self.player.y)
+        
+
+        # Desenhar o raio de ataque
+        pyxel.rect(rect_x, rect_y, rect_width, rect_height, 9)
+
+        # Verificar colisão com inimigos
+        for entity in self.entities:
+            if self.check_collision(entity, attack_end_x, attack_end_y):
+                print("Inimigo atingido!")
+
+    def check_collision(self, entity, x, y):
+        enemy_size = 16
+        if (x > entity.x and x < entity.x + enemy_size and
+            y > entity.y and y < entity.y + enemy_size):
+            return True
+        return False
         
 
 App()
