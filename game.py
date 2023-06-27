4d955fe6f1931117d2875ee5befd574e3d0df53d
bhunao <bhunao@gmail.com>
2023-06-27 17:45:36 -0300

    add: engine

---
+++ game.py
@@ -0,0 +1,126 @@
+from enum import Enum
+import pyxel
+from typing import Dict
+from engine.entities import Player, Enemy, Projectile
+
+
+class Sides(Enum):
+    LEFT = -1, 0
+    DOWN_LEFT = -1, 1
+    DOWN = 0, 1
+    DOWN_RIGHT = 1, 1
+    RIGHT = 1, 0
+    UP_RIGHT = 1, -1
+    UP = 0, -1
+    UP_LEFT = -1, -1
+
+
+LEFT = Sides.LEFT
+RIGHT = Sides.RIGHT
+UP = Sides.UP
+DOWN = Sides.DOWN
+
+FIREBALL = (
+    (2, 8, 16, 5, 8, 7),
+)
+MAGE = {
+    LEFT: ((0, 16, 0, -8, 8, 7), ),
+    RIGHT: ((0, 16, 0, 8, 8, 7), ),
+    UP: ((0, 16, 0, 8, 8, 7), ),
+    DOWN: ((0, 16, 0, 8, 8, 7), ),
+}
+ENEMIE1 = {
+    LEFT: ((0, 0, 0, 8, 8, 7),),
+    RIGHT: ((0, 0, 0, 8, 8, 7),),
+    UP: ((0, 0, 0, 8, 8, 7),),
+    DOWN: ((0, 24, 0, 8, 8, 7),),
+}
+
+
+class Game:
+    def __init__(self):
+        pyxel.init(180, 140)
+        pyxel.load('assets/pyxel.pyxres')
+        self.start()
+        pyxel.run(self.update, self.draw)
+
+    def start(self):
+        self.entities: Dict[type, list] = {}
+        self.add_entity(
+            Player(x=50, y=50, states=MAGE, current_state=DOWN),
+            Enemy(speed=1, angle=45, states=ENEMIE1, current_state=DOWN)
+        )
+
+    def add_entity(self, *entities):
+        for entity in entities:
+            if not self.entities.get(type(entity)):
+                self.entities[type(entity)] = []
+            self.entities[type(entity)].append(entity)
+
+    def remove_entity(self, *entities):
+        for entity in entities:
+            if entity in self.entities[type(entity)]:
+                self.entities[type(entity)].remove(entity)
+
+    def controller(self):
+        player: Player = self.entities[Player][0]
+        if pyxel.btn(pyxel.KEY_LEFT):
+            player.current_state = LEFT
+            player.x -= 1
+        if pyxel.btn(pyxel.KEY_RIGHT):
+            player.current_state = RIGHT
+            player.x += 1
+        if pyxel.btn(pyxel.KEY_DOWN):
+            player.y += 1
+        if pyxel.btn(pyxel.KEY_UP):
+            player.y -= 1
+        if pyxel.btnp(pyxel.KEY_SPACE):
+            self.add_entity(
+                Projectile(x=player.x, y=player.y, angle=-
+                           90, speed=3, origin=type(player),
+                           sprite=FIREBALL)
+            )
+
+    def update(self):
+        self.controller()
+
+        if len(self.entities[Enemy]) <= 10:
+            self.add_entity(
+                Enemy(
+                    states=ENEMIE1,
+                    current_state=DOWN,
+                    x=pyxel.rndi(0, pyxel.width),
+                    y=pyxel.rndi(0, pyxel.width),
+                    angle=45, speed=1
+                )
+            )
+
+        for entity_list in self.entities.values():
+            for entity in entity_list:
+                entity.move()
+
+                match entity:
+                    case Projectile():
+                        for other in self.entities[Enemy]:
+                            if entity.collide_with_target(other):
+                                self.remove_entity(entity)
+                                other.hp -= 1
+                                if other.hp <= 0:
+                                    pyxel.play(0, 0)
+                                    self.remove_entity(other)
+                        for other in self.entities[Player]:
+                            if entity.collide_with_target(other):
+                                self.remove_entity(entity)
+                                if other.hp <= 0:
+                                    self.remove_entity(other)
+                    case Enemy():
+                        entity.angle += 5
+
+    def draw(self):
+        pyxel.cls(0)
+        for entity_list in self.entities.values():
+            for entity in entity_list:
+                entity.render()
+
+
+Game()
